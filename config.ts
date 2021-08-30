643b7f21492a628ce6d00be64373203a1aac3cd1
tommyprand <tommy.prando@gmail.com>
2021-08-30 21:02:48 +0000

    Firebase config

---
+++ config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAgeZ9QdCMrO3u_8SwKxoqzyFVkoR9iF14",
+    authDomain: "marcat-6ea4a.firebaseapp.com",
+    projectId: "marcat-6ea4a",
+    storageBucket: "marcat-6ea4a.appspot.com",
+    messagingSenderId: "930993412604",
+    appId: "1:930993412604:web:31b8d51c559d6c6c017a79",
+    measurementId: "G-PPNZ3XHXR7"
+  };
